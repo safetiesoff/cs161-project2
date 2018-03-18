298af6d3b00a9fcbfed173e4e5b8d1d8683c68a0
Andrew Cui <andycui97@gmail.com>
2018-03-18 15:37:41 -0700

    tests

--- client.py
+++ client.py
@@ -117,7 +117,7 @@ class Client(BaseClient):
         message = self.crypto.symmetric_decrypt(raw[MAC_LEN+NONCE_LEN:], decode_key, cipher_name='AES', mode_name='CTR', counter=counter)
 
         if not message[:3] == 'HDR' and not message[:3] == 'MET' :
-            raise IntegrityError("Value at this location is not actually header.")
+            raise IntegrityError("Value at this location is not actually header. " + str(message[:3]))
 
         if include_metadata:
             if not message[:3] == 'MET':
@@ -132,8 +132,37 @@ class Client(BaseClient):
             else:
                 return message[3:LOC_LEN+3], message[LOC_LEN+4:LOC_LEN+ENC_KEY_LEN+4], message[LOC_LEN+ENC_KEY_LEN+4:LOC_LEN+ENC_KEY_LEN+MAC_KEY_LEN+4]
     
-    def write_metadata(self, fname, mac_key, key, value):
-        pass
+    def write_metadata(self, location, value):
+        aes_key = str(self.pks.get_encryption_key(self.username).y)[:ENC_KEY_LEN]
+        mac_key = str(self.pks.get_encryption_key(self.username).y)[:ENC_KEY_LEN]
+
+        raw = self.storage_server.get(location)             
+        if raw is None:                                  
+            raise IntegrityError("header does not exist")
+
+        mac_val = self.crypto.message_authentication_code(raw[MAC_LEN:], mac_key, hash_name='SHA256')
+        flag = (mac_val == raw[:MAC_LEN])
+        if not flag:
+            raise IntegrityError("header mac invalid")
+
+        nonce = raw[MAC_LEN:MAC_LEN+NONCE_LEN] 
+        counter = self.crypto.new_counter(COUNTER_LEN, prefix = nonce)
+
+        message = self.crypto.symmetric_decrypt(raw[MAC_LEN+NONCE_LEN:], aes_key, cipher_name='AES', mode_name='CTR', counter=counter)
+        if not message[:3] == 'MET':
+            raise IntegrityError("trying to edit metadata of invalid type")
+
+        value = message[:LOC_LEN+ENC_KEY_LEN+MAC_KEY_LEN+4] + value
+
+        aes_key = str(self.pks.get_encryption_key(self.username).y)[:ENC_KEY_LEN]
+        mac_key = str(self.pks.get_encryption_key(self.username).y)[:ENC_KEY_LEN]
+
+        nonce = self.crypto.get_random_bytes(NONCE_LEN//2)
+        counter = self.crypto.new_counter(COUNTER_LEN, prefix = nonce)
+        aes_val = self.crypto.symmetric_encrypt(value, aes_key, cipher_name='AES', mode_name='CTR', counter=counter) 
+        mac_val = self.crypto.message_authentication_code(nonce + aes_val, mac_key, hash_name='SHA256')
+ 
+        self.storage_server.put(location, mac_val+nonce+aes_val)
 
     def create_header(self, file_loc, arg1, arg2, chain=False, metadata=False):
         header_location = self.crypto.get_random_bytes(LOC_LEN)[:LOC_LEN] #idk
@@ -158,6 +187,39 @@ class Client(BaseClient):
  
         self.storage_server.put(header_location, mac_val+nonce+aes_val)
         return header_location
+
+    def edit_header(self, loc, file_loc, arg1, arg2, mac_key, decode_key):
+        raw = self.storage_server.get(loc)             
+        if raw is None:                                  
+            raise IntegrityError("header does not exist")
+
+        mac_val = self.crypto.message_authentication_code(raw[MAC_LEN:], mac_key, hash_name='SHA256')
+        flag = (mac_val == raw[:MAC_LEN])
+        if not flag:
+            raise IntegrityError("header mac invalid")
+
+        nonce = raw[MAC_LEN:MAC_LEN+NONCE_LEN] 
+        counter = self.crypto.new_counter(COUNTER_LEN, prefix = nonce)
+
+        message = self.crypto.symmetric_decrypt(raw[MAC_LEN+NONCE_LEN:], decode_key, cipher_name='AES', mode_name='CTR', counter=counter)
+
+        if not message[:3] == 'HDR' and not message[:3] == 'MET' :
+            raise IntegrityError("Value at this location is not actually header.")
+
+        if not message[LOC_LEN+3:LOC_LEN+4] == '0':
+            raise IntegrityError("edit header block is chained!" + message[LOC_LEN+3:LOC_LEN+4])
+
+        value = message[:3]+ file_loc + "0" + arg1 + arg2 + message[LOC_LEN+ENC_KEY_LEN+MAC_KEY_LEN+4:]
+
+        aes_key = str(self.pks.get_encryption_key(self.username).y)[:ENC_KEY_LEN]
+        mac_key = str(self.pks.get_encryption_key(self.username).y)[:ENC_KEY_LEN]
+
+        nonce = self.crypto.get_random_bytes(NONCE_LEN//2)
+        counter = self.crypto.new_counter(COUNTER_LEN, prefix = nonce)
+        aes_val = self.crypto.symmetric_encrypt(value, aes_key, cipher_name='AES', mode_name='CTR', counter=counter) 
+        mac_val = self.crypto.message_authentication_code(nonce + aes_val, mac_key, hash_name='SHA256')
+ 
+        self.storage_server.put(loc, mac_val+nonce+aes_val)
 
     def read_file(self, location, mac_key, decode_key):
         raw = self.storage_server.get(location)             
@@ -195,6 +257,7 @@ class Client(BaseClient):
             file_key = self.crypto.get_random_bytes(ENC_KEY_LEN)[:ENC_KEY_LEN]
             file_mac_key = self.crypto.get_random_bytes(MAC_KEY_LEN)[:MAC_KEY_LEN]
             header_location = self.create_header(file_location, file_key, file_mac_key, chain=False, metadata=True)
+            self.write_metadata(header_location, self.username + "," + header_location)
             self.create_pointer(fname, header_location, self.username)
 
         else:
@@ -235,17 +298,43 @@ class Client(BaseClient):
 
         if not metadata is None:
             new_header_location = self.create_header(file_location, file_key, file_mac_key, chain=False, metadata=False)
-            pass # update metadata
+            self.write_metadata(header_location, metadata+"," + user + "," + new_header_location)
         else:
             new_header_location = self.create_header(header_location, file_owner_uname, "", chain=True, metadata=False)
 
         return new_header_location
-
 
     def receive_share(self, from_username, newname, message):
         if message == "":
             return
         self.create_pointer(newname, message, from_username)
 
-    def revoke(self, user, name):
-        pass
+    def revoke(self, user, fname):
+        header_location, file_owner_uname = self.read_verify_ptr(self.get_pointer_loc(fname), self.get_pointer_mac(fname), self.get_pointer_key(fname))
+        enc_key = str(self.pks.get_encryption_key(file_owner_uname).y)[:ENC_KEY_LEN]
+        mac_key = str(self.pks.get_encryption_key(file_owner_uname).y)[:ENC_KEY_LEN]
+        file_location, file_key, file_mac_key, metadata  = self.read_verify_header(header_location,enc_key, mac_key, include_metadata=True)
+        if metadata is None:
+            raise IntegrityError("trying to share file that isn't yours")
+
+        list_users = metadata.split(",")
+        index = list_users.index(user)
+        del list_users[index]
+        del list_users[index]
+
+        metadata = ",".join(list_users)
+        self.write_metadata(header_location, metadata)
+        
+        file_contents = self.read_file(file_location, file_mac_key, file_key)
+
+        file_location = self.crypto.get_random_bytes(LOC_LEN)[:LOC_LEN]
+        file_key = self.crypto.get_random_bytes(ENC_KEY_LEN)[:ENC_KEY_LEN]
+        file_mac_key = self.crypto.get_random_bytes(MAC_KEY_LEN)[:MAC_KEY_LEN]
+
+        self.write_file(file_location, file_contents, file_mac_key, file_key)
+
+        for i in range(len(list_users)):
+            if i%2 == 1:
+                loc = list_users[i]
+                self.edit_header(loc, file_location, file_key, file_mac_key, enc_key, mac_key)
+
